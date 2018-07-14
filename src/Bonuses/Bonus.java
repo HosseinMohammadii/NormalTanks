ea061f27dc4891f4b9d63b4d18f457b184ae39a3
kamyar <kamyarkh76@gmail.com>
2018-07-14 21:22:49 +0430

    write javadoc

--- src/Bonuses/Bonus.java
+++ src/Bonuses/Bonus.java
@@ -1,5 +1,9 @@
 package Bonuses;
-//
+/**
+ * this class is for bonuses and has fiels like
+ * x,y,type,...
+ *
+ */
 public abstract class Bonus {
     protected int x;
     protected int y;
@@ -11,7 +15,11 @@ public abstract class Bonus {
     protected int toShowY;
 
     private boolean exist;
-
+    /**
+     * its constructor that set its position
+     * @param x
+     * @param y
+     */
     public Bonus(int x, int y) {
         this.x = x;
         this.y = y;
@@ -19,38 +27,67 @@ public abstract class Bonus {
         updateToShow();
     }
 
+    /**
+     *  /**
+     * whe bonus ate it set false
+     */
+
     public void eat() {
         exist = false;
     }
+    /**
+     * to give rhe centre of image
+     */
     private void updateToShow(){
         toShowX=x-25;
         toShowY=y-25;
     }
-
+    /**
+     * get its type
+     * @return
+     */
     public int getType() {
         return type;
     }
-
+    /**
+     * to return its capacity
+     * @return
+     */
     public int giveBonus(){
         return capacity;
     }
-
+    /**
+     * for getting toShowX
+     * @return
+     */
     public int getToShowX() {
         return toShowX;
     }
-
+    /**
+     * for getting toShowY
+     * @return
+     */
     public int getToShowY() {
         return toShowY;
     }
-
+    /**
+     * for getting exist
+     * @return
+     */
     public boolean isExist() {
         return exist;
     }
-
+    /**
+     * for getting X
+     * @return
+     */
     public int getX() {
         return x;
     }
-
+    /**
+     * for getting Y
+     * @return
+     */
     public int getY() {
         return y;
     }
